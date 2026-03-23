e27d3e5d77235009551530cf1b23215517b81bac
jaycem <45575946+jaycem-dev@users.noreply.github.com>
2026-03-23 18:18:02 -0300

    chore: inline post functions

--- modules/post.sh
+++ modules/post.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+setup_keymap() {
+	echo "==> Configuring keymap..."
+	mkdir -p "$MNT_DIR/etc"
+	if grep -q "^KEYMAP=" "$MNT_DIR/etc/rc.conf" 2>/dev/null; then
+		sed -i 's|^KEYMAP=.*|KEYMAP="i386/colemak/mod-dh-iso-us"|' "$MNT_DIR/etc/rc.conf"
+	else
+		echo 'KEYMAP="i386/colemak/mod-dh-iso-us"' >>"$MNT_DIR/etc/rc.conf"
+	fi
+}
+
 post_main() {
 	echo ""
-	warn "This assumes the system was installed using this script"
+	warn "This assumes the system was installed using this script."
 	echo "==> Opening LUKS container..."
 	cryptsetup luksOpen /dev/"${DISK}2" "$VG_NAME"
 
@@ -11,5 +21,9 @@ post_main() {
 
 	mount_filesystems
 
-	echo "==> Post-install (placeholder)"
+	echo "==> Running post-install configuration..."
+
+	setup_keymap
+
+	echo "==> Post-install configuration complete"
 }
