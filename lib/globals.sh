6309c5f909297ce5da9bc2c93f3af52b2c10a9de
jaycem <45575946+jaycem-dev@users.noreply.github.com>
2026-03-25 11:17:50 -0300

    chore: change default remote

--- lib/globals.sh
+++ lib/globals.sh
@@ -3,7 +3,7 @@
 VG_NAME="${VG_NAME:-cryptroot}"
 EFI_SIZE="${EFI_SIZE:-1G}"
 SWAP_SIZE="${SWAP_SIZE:-4G}"
-REPO_URL="${REPO_URL:-https://repo-fastly.voidlinux.org/current}"
+REPO_URL="${REPO_URL:-https://repo-default.voidlinux.org/current}"
 MNT_DIR="${MNT_DIR:-/mnt}"
 
 DISK=""
