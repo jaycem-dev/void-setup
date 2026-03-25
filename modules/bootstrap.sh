6e2a103077ae9d1eed064df6550565a5ffa3e9ec
jaycem <45575946+jaycem-dev@users.noreply.github.com>
2026-03-25 14:05:14 -0300

    feat: chpasswd for non interactive setup
    joined several xchroot calls per function

--- modules/bootstrap.sh
+++ modules/bootstrap.sh
@@ -1,223 +1,212 @@
 #!/usr/bin/env bash
 
 configure_system() {
-	echo "==> Configuring system..."
+    echo "==> Configuring system..."
 
-	echo "$HOSTNAME" >"$MNT_DIR"/etc/hostname
-	echo "LANG=en_US.UTF-8" >"$MNT_DIR"/etc/locale.conf
-	echo "en_US.UTF-8 UTF-8" >>"$MNT_DIR"/etc/default/libc-locales
+    echo "$HOSTNAME" >"$MNT_DIR"/etc/hostname
+    echo "LANG=en_US.UTF-8" >"$MNT_DIR"/etc/locale.conf
+    echo "en_US.UTF-8 UTF-8" >>"$MNT_DIR"/etc/default/libc-locales
 
-	xchroot "$MNT_DIR" bash -c '
-		if grep -q "^GRUB_ENABLE_CRYPTODISK=" /etc/default/grub; then
-			sed -i "s/^#\?GRUB_ENABLE_CRYPTODISK=.*/GRUB_ENABLE_CRYPTODISK=y/" /etc/default/grub
-		else
-			echo "GRUB_ENABLE_CRYPTODISK=y" >> /etc/default/grub
-		fi
-	'
+    grep -q "^GRUB_ENABLE_CRYPTODISK=" "$MNT_DIR"/etc/default/grub || echo "GRUB_ENABLE_CRYPTODISK=y" >>"$MNT_DIR"/etc/default/grub
+    sed -i "s|^GRUB_CMDLINE_LINUX_DEFAULT=.*|GRUB_CMDLINE_LINUX_DEFAULT=\"quiet loglevel=3 rd.luks.uuid=$LUKS_UUID rd.lvm.vg=$VG_NAME\"|" "$MNT_DIR"/etc/default/grub
 
-	xchroot "$MNT_DIR" bash -c "
-		if grep -q \"^GRUB_CMDLINE_LINUX_DEFAULT=\" /etc/default/grub; then
-			sed -i \"s|^#\?GRUB_CMDLINE_LINUX_DEFAULT=.*|GRUB_CMDLINE_LINUX_DEFAULT=\\\"quiet loglevel=3 rd.luks.uuid=$LUKS_UUID rd.lvm.vg=$VG_NAME\\\"|\" /etc/default/grub
-		else
-			echo \"GRUB_CMDLINE_LINUX_DEFAULT=\\\"quiet loglevel=3 rd.luks.uuid=$LUKS_UUID rd.lvm.vg=$VG_NAME\\\"\" >> /etc/default/grub
-		fi
-	"
-
-	echo "==> System configuration complete"
+    echo "==> System configuration complete"
 }
 
 setup_users() {
-	echo "==> Setting up users..."
+    echo "==> Setting up users..."
 
-	xchroot "$MNT_DIR" useradd -m -G wheel,users,audio,video,kvm,xbuilder,network "$USERNAME"
+    xchroot "$MNT_DIR" bash -c "
+        useradd -m -G wheel,users,audio,video,kvm,xbuilder,network $USERNAME
+        echo $USERNAME:$ROOT_PASS | chpasswd -m
+        echo root:$ROOT_PASS | chpasswd -m
+    "
 
-	xchroot "$MNT_DIR" bash -c "echo 'root:$ROOT_PASS' | chpasswd"
-	xchroot "$MNT_DIR" bash -c "echo '$USERNAME:$ROOT_PASS' | chpasswd"
+    sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' "$MNT_DIR"/etc/sudoers
 
-	sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' "$MNT_DIR"/etc/sudoers
-
-	echo "==> User setup complete"
+    echo "==> User setup complete"
 }
 
 install_bootloader() {
-	echo "==> Installing GRUB..."
+    echo "==> Installing GRUB..."
 
-	xchroot "$MNT_DIR" grub-install "/dev/$DISK"
+    xchroot "$MNT_DIR" bash -c "
+		grub-install /dev/$DISK
+		xbps-reconfigure -fa
+	"
 
-	echo "==> Generating initramfs..."
-	xchroot "$MNT_DIR" xbps-reconfigure -fa
-
-	echo "==> Bootloader installation complete"
+    echo "==> Bootloader installation complete"
 }
 
 setup_network() {
-	echo "==> Setting up network..."
+    echo "==> Setting up network..."
 
-	mkdir -p "$MNT_DIR"/etc/NetworkManager/conf.d
-	echo '[device]
+    mkdir -p "$MNT_DIR"/etc/NetworkManager/conf.d
+    echo '[device]
 wifi.backend=iwd' >"$MNT_DIR"/etc/NetworkManager/conf.d/wifi_backend.conf
+    mkdir -p "$MNT_DIR"/etc/runit/runsvdir/default
 
-	xchroot "$MNT_DIR" mkdir -p /etc/runit/runsvdir/default
-	xchroot "$MNT_DIR" ln -s /etc/sv/dbus /etc/runit/runsvdir/default/dbus
-	xchroot "$MNT_DIR" ln -s /etc/sv/NetworkManager /etc/runit/runsvdir/default/NetworkManager
+    xchroot "$MNT_DIR" bash -c "
+		ln -s /etc/sv/dbus /etc/runit/runsvdir/default/dbus
+		ln -s /etc/sv/NetworkManager /etc/runit/runsvdir/default/NetworkManager
+	"
 
-	echo "==> Network setup complete"
+    echo "==> Network setup complete"
 }
 
 prompt_hostname() {
-	read -rp "Hostname (default: void): " hostname_input
-	HOSTNAME="${hostname_input:-void}"
+    read -rp "Hostname (default: void): " hostname_input
+    HOSTNAME="${hostname_input:-void}"
 }
 
 prompt_users() {
-	read -rp "Regular user: " USERNAME
-	[[ -n "$USERNAME" ]] || die "Username is required"
-	while true; do
-		read -srp "Password (used for root, user and encryption): " ROOT_PASS
-		echo ""
-		read -srp "Confirm password: " ROOT_PASS_CONFIRM
-		echo ""
-		[[ "$ROOT_PASS" = "$ROOT_PASS_CONFIRM" ]] && break
-		echo "Passwords do not match. Try again."
-	done
+    read -rp "Regular user: " USERNAME
+    [[ -n "$USERNAME" ]] || die "Username is required"
+    while true; do
+        read -srp "Password (used for root, user and encryption): " ROOT_PASS
+        echo ""
+        read -srp "Confirm password: " ROOT_PASS_CONFIRM
+        echo ""
+        [[ "$ROOT_PASS" = "$ROOT_PASS_CONFIRM" ]] && break
+        echo "Passwords do not match. Try again."
+    done
 }
 
 check_dependencies() {
-	check_command sfdisk
-	check_command cryptsetup
-	check_command lvm
-	check_command mkfs.vfat
-	check_command mkfs.btrfs
-	check_command xbps-install
-	check_command xchroot
-	check_command xgenfstab
+    check_command sfdisk
+    check_command cryptsetup
+    check_command lvm
+    check_command mkfs.vfat
+    check_command mkfs.btrfs
+    check_command xbps-install
+    check_command xchroot
+    check_command xgenfstab
 }
 
 partition_disk() {
-	local disk_path="$1"
-	echo "==> Partitioning $disk_path..."
+    local disk_path="$1"
+    echo "==> Partitioning $disk_path..."
 
-	sfdisk --wipe always "$DISK_PATH" <<EOF
+    sfdisk --wipe always "$DISK_PATH" <<EOF
 label: gpt
 /dev/${DISK}1 : start=2048, size=${EFI_SIZE}, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B, name="EFI"
 /dev/${DISK}2 : type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, name="Linux"
 EOF
 
-	echo "==> Partitioning complete"
+    echo "==> Partitioning complete"
 }
 
 setup_luks() {
-	local luks_dev="$1"
-	echo "==> Setting up LUKS encryption on $luks_dev"
+    local luks_dev="$1"
+    echo "==> Setting up LUKS encryption on $luks_dev"
 
-	echo "$ROOT_PASS" | cryptsetup luksFormat --type luks1 "$luks_dev"
+    echo "$ROOT_PASS" | cryptsetup luksFormat --type luks1 "$luks_dev"
 
-	echo "==> Opening LUKS container..."
-	echo "$ROOT_PASS" | cryptsetup luksOpen "$luks_dev" "$VG_NAME"
+    echo "==> Opening LUKS container..."
+    echo "$ROOT_PASS" | cryptsetup luksOpen "$luks_dev" "$VG_NAME"
 
-	LUKS_UUID=$(blkid -o value -s UUID "$luks_dev")
+    LUKS_UUID=$(blkid -o value -s UUID "$luks_dev")
 
-	echo "==> LUKS setup complete"
+    echo "==> LUKS setup complete"
 }
 
 setup_lvm() {
-	echo "==> Creating LVM volumes..."
+    echo "==> Creating LVM volumes..."
 
-	vgcreate "$VG_NAME" /dev/mapper/"$VG_NAME"
+    vgcreate "$VG_NAME" /dev/mapper/"$VG_NAME"
 
-	lvcreate --name swap -L "$SWAP_SIZE" "$VG_NAME"
-	lvcreate --name root -l 100%FREE "$VG_NAME"
+    lvcreate --name swap -L "$SWAP_SIZE" "$VG_NAME"
+    lvcreate --name root -l 100%FREE "$VG_NAME"
 
-	echo "==> LVM setup complete"
+    echo "==> LVM setup complete"
 }
 
 create_filesystems() {
-	echo "==> Creating filesystems..."
+    echo "==> Creating filesystems..."
 
-	mkfs.vfat -F 32 -n EFI /dev/"${DISK}1"
+    mkfs.vfat -F 32 -n EFI /dev/"${DISK}1"
 
-	mkfs.btrfs -L root /dev/"$VG_NAME"/root
+    mkfs.btrfs -L root /dev/"$VG_NAME"/root
 
-	mount /dev/"$VG_NAME"/root "$MNT_DIR"
-	btrfs subvolume create "$MNT_DIR"/@
-	btrfs subvolume create "$MNT_DIR"/@home
-	btrfs subvolume create "$MNT_DIR"/@snapshots
-	umount "$MNT_DIR"
+    mount /dev/"$VG_NAME"/root "$MNT_DIR"
+    btrfs subvolume create "$MNT_DIR"/@
+    btrfs subvolume create "$MNT_DIR"/@home
+    btrfs subvolume create "$MNT_DIR"/@snapshots
+    umount "$MNT_DIR"
 
-	mkswap -L swap /dev/"$VG_NAME"/swap
-	swapon /dev/"$VG_NAME"/swap
+    mkswap -L swap /dev/"$VG_NAME"/swap
+    swapon /dev/"$VG_NAME"/swap
 
-	echo "==> Filesystems created"
+    echo "==> Filesystems created"
 }
 
 install_base() {
-	echo "==> Installing base system from $REPO_URL..."
+    echo "==> Installing base system from $REPO_URL..."
 
-	mkdir -p "$MNT_DIR"/var/db/xbps/keys
-	cp /var/db/xbps/keys/* "$MNT_DIR"/var/db/xbps/keys/
+    mkdir -p "$MNT_DIR"/var/db/xbps/keys
+    cp /var/db/xbps/keys/* "$MNT_DIR"/var/db/xbps/keys/
 
-	xbps-install -Sy -R "$REPO_URL" -r "$MNT_DIR" \
-		void-repo-nonfree \
-		base-system \
-		lvm2 \
-		cryptsetup \
-		grub-x86_64-efi \
-		btrfs-progs \
-		xtools \
-		NetworkManager \
-		iwd \
-		shadow
+    xbps-install -Sy -R "$REPO_URL" -r "$MNT_DIR" \
+        void-repo-nonfree \
+        base-system \
+        lvm2 \
+        cryptsetup \
+        grub-x86_64-efi \
+        btrfs-progs \
+        xtools \
+        NetworkManager \
+        iwd
 
-	echo "==> Base system installed"
+    echo "==> Base system installed"
 }
 
 generate_fstab() {
-	echo "==> Generating fstab..."
-	xgenfstab -p "$MNT_DIR" >"$MNT_DIR"/etc/fstab
-	echo "==> fstab generated"
+    echo "==> Generating fstab..."
+    xgenfstab -p "$MNT_DIR" >"$MNT_DIR"/etc/fstab
+    echo "==> fstab generated"
 }
 
 setup_luks_keyfile() {
-	echo "==> Setting up keyfile for automatic unlock..."
+    echo "==> Setting up keyfile for automatic unlock..."
 
-	dd bs=1 count=64 if=/dev/urandom of="$MNT_DIR"/boot/volume.key
-	chmod 000 "$MNT_DIR"/boot/volume.key
+    dd bs=1 count=64 if=/dev/urandom of="$MNT_DIR"/boot/volume.key
+    chmod 000 "$MNT_DIR"/boot/volume.key
 
-	echo "$ROOT_PASS" | cryptsetup luksAddKey /dev/"${DISK}2" "$MNT_DIR"/boot/volume.key
+    echo "$ROOT_PASS" | cryptsetup luksAddKey /dev/"${DISK}2" "$MNT_DIR"/boot/volume.key
 
-	chmod -R go-rwx "$MNT_DIR"/boot
+    chmod -R go-rwx "$MNT_DIR"/boot
 
-	echo "$VG_NAME UUID=$LUKS_UUID /boot/volume.key luks" >>"$MNT_DIR"/etc/crypttab
+    echo "$VG_NAME UUID=$LUKS_UUID /boot/volume.key luks" >>"$MNT_DIR"/etc/crypttab
 
-	mkdir -p "$MNT_DIR"/etc/dracut.conf.d
-	echo 'install_items+=" /boot/volume.key /etc/crypttab "' >"$MNT_DIR"/etc/dracut.conf.d/10-crypt.conf
+    mkdir -p "$MNT_DIR"/etc/dracut.conf.d
+    echo 'install_items+=" /boot/volume.key /etc/crypttab "' >"$MNT_DIR"/etc/dracut.conf.d/10-crypt.conf
 
-	echo "==> Keyfile setup complete"
+    echo "==> Keyfile setup complete"
 }
 
 bootstrap_main() {
-	check_root
-	check_dependencies
-	detect_env
+    check_root
+    check_dependencies
+    detect_env
 
-	prompt_disk
-	prompt_hostname
-	prompt_users
+    prompt_disk
+    prompt_hostname
+    prompt_users
 
-	partition_disk "$DISK_PATH"
-	setup_luks "${DISK_PATH}2"
-	setup_lvm
-	create_filesystems
-	mount_filesystems
-	install_base
-	generate_fstab
-	configure_system
-	setup_luks_keyfile
-	install_bootloader
-	setup_network
-	setup_users
+    partition_disk "$DISK_PATH"
+    setup_luks "${DISK_PATH}2"
+    setup_lvm
+    create_filesystems
+    mount_filesystems
+    install_base
+    generate_fstab
+    configure_system
+    setup_luks_keyfile
+    install_bootloader
+    setup_network
+    setup_users
 
-	echo ""
-	echo "==> Bootstrap complete! System ready for post-install configuration."
-	echo "==> Mount point: $MNT_DIR"
+    echo ""
+    echo "==> Bootstrap complete! System ready for post-install configuration."
+    echo "==> Mount point: $MNT_DIR"
 }
