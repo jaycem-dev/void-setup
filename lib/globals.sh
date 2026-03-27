2713a3f2cd43eb4a0c6135383461ecfd77d01840
jaycem <45575946+jaycem-dev@users.noreply.github.com>
2026-03-26 23:27:13 -0300

    feat: add ARCH and BTRFS_OPTS configuration variables

--- lib/globals.sh
+++ lib/globals.sh
@@ -5,6 +5,8 @@ EFI_SIZE="${EFI_SIZE:-1G}"
 SWAP_SIZE="${SWAP_SIZE:-4G}"
 REPO_URL="${REPO_URL:-https://repo-default.voidlinux.org/current}"
 MNT_DIR="${MNT_DIR:-/mnt}"
+ARCH="${ARCH:-x86_64}"
+BTRFS_OPTS="${BTRFS_OPTS:-compress=zstd,noatime,discard=async}"
 
 DISK=""
 DISK_PATH=""
